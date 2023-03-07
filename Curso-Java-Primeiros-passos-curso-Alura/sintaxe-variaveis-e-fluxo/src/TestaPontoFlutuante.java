422daac8c83e735a59b7cc959e790ec3d4f0ef6d
Cesar Iglesias <cesaribneto.job@gmail.com>
2023-03-07 10:32:28 -0300

    Java-Curso-Alura

---
+++ Curso-Java-Primeiros-passos-curso-Alura/sintaxe-variaveis-e-fluxo/src/TestaPontoFlutuante.java
@@ -0,0 +1,17 @@
+public class TestaPontoFlutuante{
+
+    public static void main(String[] args){
+        //eu usso o double como uma var mas para numero quebrado
+        double salario = 1250.70;
+
+        System.out.println("meu salario é " + salario);
+        
+        double divisao = 5.0 / 2;
+
+        System.out.println("divisao: " + divisao);
+
+
+        double numero = 30.5 / 2;
+        System.out.println((int) numero + (int) numero);
+    }
+}
